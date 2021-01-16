f3732a39f1641ebbe82142286bf5e2f4c1432070
Sean <73643889+seanwpaterson@users.noreply.github.com>
2021-01-16 18:51:04 +0000

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
-randomNumber1 = Math.floor(Math.random() * 6) + 1;
-randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector("button").addEventListener("click", function() {
+  randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-document.getElementById('die1').src = "images/dice"+randomNumber1+".png";
-document.getElementById('die2').src = "images/dice"+randomNumber2+".png";
+  document.getElementById('die1').src = "images/dice" + randomNumber1 + ".png";
+  document.getElementById('die2').src = "images/dice" + randomNumber2 + ".png";
 
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("#content h1").innerHTML = "🚩 Player 1 wins!";
-} else if (randomNumber1 < randomNumber2) {
-  document.querySelector("#content h1").innerHTML = "Player 2 wins! 🚩";
-} else {
-  document.querySelector("#content h1").innerHTML = "It's a DRAW!"
-}
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("#content h1").innerHTML = "🚩 Player 1 wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("#content h1").innerHTML = "Player 2 wins! 🚩";
+  } else {
+    document.querySelector("#content h1").innerHTML = "It's a DRAW!"
+  }
+});
